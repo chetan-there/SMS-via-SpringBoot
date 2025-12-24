b8ff56f021eaf88a365eba8ec506facdc0cd7a91
chetan-there <therechetan24@gmail.com>
2025-12-24 13:29:30 +0530

    Today i create the global Exception Handler class , also i create common layout pages

---
+++ studentmanagment/src/main/java/com/ct/studentmanagment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.ct.studentmanagment.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.csrf.CsrfException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(CsrfException.class)
+	public String csrfExceptionHandler(RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("message","session expired ,please log in again !!");
+		return "";
+	}
+	
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public String genericExceptionHandler() {
+		return "500";
+	}
+
+}
